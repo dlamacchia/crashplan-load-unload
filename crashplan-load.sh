9b84c0db33a6d2ec02222e837b5635e0ce3938ab
David LaMacchia <dlamacchia@twitter.com>
2015-04-12 11:36:00 -0700

    Updated to present osx notifications via osascript

--- crashplan-load.sh
+++ crashplan-load.sh
@@ -9,113 +9,91 @@
 #	%admin ALL=NOPASSWD: /bin/launchctl list
 #	%admin ALL=NOPASSWD: /bin/launchctl load /Library/LaunchDaemons/com.crashplan.engine.plist
 # 	%admin ALL=NOPASSWD: /bin/launchctl unload /Library/LaunchDaemons/com.crashplan.engine.plist
+#
+# [dml]: update 04/12/15 with formatting and support for osx notifications via applescript
 
 NAME="$0:t"
+PLIST="/Library/LaunchDaemons/com.crashplan.engine.plist"
 
-PLIST='/Library/LaunchDaemons/com.crashplan.engine.plist’
-
-	# if the file doesn't exist, stop right there
+# if the file doesn't exist, stop right there
 [[ -e "$PLIST" ]] || exit 0
 
-
-GROWL_APP='CrashPlan'
-
 msg () {
-	STICKY=""
-	DIE='no'
+    STICKY=""
+    DIE='no'
 
-	for ARGS in "$@"
-	do
-		case "$ARGS" in
-			-s|--sticky)
-				STICKY=--sticky
-				shift
-			;;
+    for ARGS in "$@"
+    do
+	case "$ARGS" in
+	    -s|--sticky)
+		STICKY=--sticky
+		shift
+		;;
 
-			-d|--die)
-				DIE=yes
-				STICKY=--sticky
-				shift
-			;;
-		esac
-	done
-
-	MSG="$@"
-
-	ps cx | egrep -q ' Growl$' || open -g -a Growl 2>/dev/null
-
-	ps cx | egrep -q ' Growl$' && \
-	(( $+commands[growlnotify] )) && growlnotify --appIcon "$APPNAME" --identifier "${NAME}.$$" ${STICKY} --message "$MSG" "$NAME"
-
-	echo "$NAME: $MSG"
-
-	[[ "$DIE" = "yes" ]] && exit 1
+	    -d|--die)
+		DIE=yes
+		STICKY=--sticky
+		shift
+		;;
+	esac
+    done
+    MSG="$@"
+    osascript \
+        -e "on run(argv)" \
+        -e "return display notification item 1 of argv" \
+        -e "end" \
+        -- "$NAME: $MSG"
+    echo "$NAME: $MSG"
+    [[ "$DIE" = "yes" ]] && exit 1
 }
 
 launch_menu_bar_app () {
-
-if [ "`ps cx | egrep ' CrashPlan menu bar$'`" = "" ]
-then
-		# the CrashPlan menu bar is NOT running
-
-		msg "Telling CrashPlan menu bar app to launch"
-
-		open -a "CrashPlan menu bar"
-fi
-
+    if [ "`ps cx | egrep ' CrashPlan menu bar$'`" = "" ]
+    then
+        # the CrashPlan menu bar is NOT running
+        msg "Telling CrashPlan menu bar app to launch"
+        open -a "CrashPlan menu bar"
+    fi
 }
 
 launch_menu_bar_app
 
-	# Get a list of all of root's loaded plists
+# Get a list of all of root's loaded plists
 ROOT_LOADED=$(sudo launchctl list)
 
 if [[ "$ROOT_LOADED" == "" ]]
 then
-			# if we didn't get anything in response, die ugly
-		msg --die "ROOT_LOADED is empty"
-		exit 1
+    # if we didn't get anything in response, die ugly
+    msg --die "ROOT_LOADED is empty"
+    exit 1
 fi
 
-	# if we got this far, we did get some response from ROOT_LOADED
-
+# if we got this far, we did get some response from ROOT_LOADED
 echo "$ROOT_LOADED" | fgrep -q com.crashplan.engine
-
 EXIT="$?"
-
 if [[ "$EXIT" = "0" ]]
 then
-			# If we get here then crashplan IS loaded
-			# so we exit because this is what we wanted
-		msg "CrashPlan already loaded"
-
-		launch_menu_bar_app
-
-		exit 0
-
+    # If we get here then crashplan IS loaded
+    # so we exit because this is what we wanted
+    msg "CrashPlan already loaded"
+    launch_menu_bar_app
+    exit 0
 else
-			# if we get here, crashplan is not loaded
-			# so we'll try to load it and exit if successful
-		EXIT=1
-		sudo /bin/launchctl load "$PLIST" && EXIT=0
-
-		if [ "$EXIT" = "0" ]
-		then
-				msg "Loaded crashplan plist successfully"
-
-					# When running, I want the menu bar also running
-				launch_menu_bar_app
-
-		else
-				msg --die "Failed to Load plist"
-		fi
-
-		exit $EXIT
-
+    # if we get here, crashplan is not loaded
+    # so we'll try to load it and exit if successful
+    EXIT=1
+    sudo /bin/launchctl load "$PLIST" && EXIT=0
+    if [ "$EXIT" = "0" ]
+    then
+	msg "Loaded crashplan plist successfully"
+	# When running, I want the menu bar also running
+	launch_menu_bar_app
+    else
+	msg --die "Failed to Load plist"
+    fi
+    exit $EXIT
 fi
 
-	# If we get here, something unexpected happened
+# If we get here, something unexpected happened
 exit 2
-
-#
 #EOF
--- crashplan-unload.sh
+++ crashplan-unload.sh
@@ -9,100 +9,85 @@
 #	%admin ALL=NOPASSWD: /bin/launchctl list
 #	%admin ALL=NOPASSWD: /bin/launchctl load /Library/LaunchDaemons/com.crashplan.engine.plist
 # 	%admin ALL=NOPASSWD: /bin/launchctl unload /Library/LaunchDaemons/com.crashplan.engine.plist
+#
+# [dml]: update 04/12/15 with formatting and support for osx notifications via applescript
 
 NAME="$0:t"
+PLIST="/Library/LaunchDaemons/com.crashplan.engine.plist"
 
-PLIST='/Library/LaunchDaemons/com.crashplan.engine.plist’
-
-	# if the file doesn't exist, stop right there
+# if the file doesn't exist, stop right there
 [[ -e "$PLIST" ]] || exit 0
 
-GROWL_APP='CrashPlan'
-
 msg () {
-	STICKY=""
-	DIE='no'
+    STICKY=""
+    DIE='no'
+    for ARGS in "$@"
+    do
+	case "$ARGS" in
+	    -s|--sticky)
+		STICKY=--sticky
+		shift
+		;;
 
-	for ARGS in "$@"
-	do
-		case "$ARGS" in
-			-s|--sticky)
-				STICKY=--sticky
-				shift
-			;;
-
-			-d|--die)
-				DIE=yes
-				STICKY=--sticky
-				shift
-			;;
-		esac
-	done
-
-	MSG="$@"
-
-	ps cx | egrep -q ' Growl$' || open -g -a Growl 2>/dev/null
-
-	ps cx | egrep -q ' Growl$' && \
-	(( $+commands[growlnotify] )) && growlnotify --appIcon "$APPNAME" --identifier "${NAME}.$$" ${STICKY} --message "$MSG" "$NAME"
-
-	echo "$NAME: $MSG"
-
-	[[ "$DIE" = "yes" ]] && exit 1
+	    -d|--die)
+		DIE=yes
+		STICKY=--sticky
+		shift
+		;;
+	esac
+    done
+    MSG="$@"
+    osascript \
+        -e "on run(argv)" \
+        -e "return display notification item 1 of argv" \
+        -e "end" \
+        -- "$NAME: $MSG"
+    echo "$NAME: $MSG"
+    [[ "$DIE" = "yes" ]] && exit 1
 }
 
 if [ "`ps cx | egrep ' CrashPlan menu bar$'`" != "" ]
 then
-		# the CrashPlan menu bar is running
-
-		msg "Telling CrashPlan menu bar app to quit"
-
-		osascript -e 'tell application "CrashPlan menu bar" to quit'
+    # the CrashPlan menu bar is running
+    msg "Telling CrashPlan menu bar app to quit"
+    osascript -e 'tell application "CrashPlan menu bar" to quit'
 fi
 
 
-	# Get a list of all of root's loaded plists
+# Get a list of all of root's loaded plists
 ROOT_LOADED=$(sudo launchctl list)
 
 if [[ "$ROOT_LOADED" == "" ]]
 then
-			# if we didn't get anything in response, die ugly
-		msg --die "ROOT_LOADED is empty"
-		exit 1
+    # if we didn't get anything in response, die ugly
+    msg --die "ROOT_LOADED is empty"
+    exit 1
 fi
 
-	# if we got this far, we did get some response from ROOT_LOADED
+# if we got this far, we did get some response from ROOT_LOADED
 
 echo "$ROOT_LOADED" | fgrep -q com.crashplan.engine
-
 EXIT="$?"
 
 if [[ "$EXIT" = "0" ]]
 then
+    EXIT=1
+    sudo /bin/launchctl unload "$PLIST" && EXIT=0
+    if [ "$EXIT" = "0" ]
+    then
+	msg "UN-loaded CrashPlan plist successfully"
+    else
+	msg --die "Failed to unload CrashPlan"
+    fi
 
-		EXIT=1
-		sudo /bin/launchctl unload "$PLIST" && EXIT=0
-
-		if [ "$EXIT" = "0" ]
-		then
-				msg "UN-loaded CrashPlan plist successfully"
-		else
-				msg --die "Failed to unload CrashPlan"
-		fi
-
-		exit $EXIT
-
+    exit $EXIT
 else
-			# if we get here, crashplan is not loaded, so we'll exit, because we have the state we wanted
-
-		msg "CrashPlan already unloaded"
-
-		exit 0
+    # if we get here, crashplan is not loaded, so we'll exit, because we have the state we wanted
+    msg "CrashPlan already unloaded"
+    exit 0
 fi
 
-
-	# If we get here, something unexpected happened
+# If we get here, something unexpected happened
 exit 2
 
-#
 #EOF
